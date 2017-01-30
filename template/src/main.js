cc4a57ae6d1287d9e59d2860b09f235e6e419d4a
David DANNEKER <danneker@gmail.com>
2017-01-30 16:03:09 +0100

    Add store to Vue

--- template/src/main.js
+++ template/src/main.js
@@ -10,12 +10,14 @@ import { mapActions } from 'vuex'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  store,
   {{#if_eq build "runtime"}}
   render: h => h(App){{#if_eq lintConfig "airbnb"}},{{/if_eq}}
   {{/if_eq}}
   {{#if_eq build "standalone"}}
   template: '<App/>',
   components: { App },
+  {{/if_eq}}
   methods: {
     ...mapActions([
       'install',
@@ -26,5 +28,4 @@ new Vue({
     this.install(){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
     this.activate(){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
   }{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
-  {{/if_eq}}
 }){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
